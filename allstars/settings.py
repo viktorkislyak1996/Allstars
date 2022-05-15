c4f680ef8e0dcedbdc3ca353839fc4fa70e67a36
VIktor Kislyak <105554514+ViktorKislyak1996@users.noreply.github.com>
2022-05-15 12:47:37 +0300

    Update settings.py

--- allstars/settings.py
+++ allstars/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j9&$cs1ysf_li^(wd(b*ni(khkg1p=2l4&q@=ipyadcbzb!e%^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
